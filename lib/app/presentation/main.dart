dc0d005ec5f38e6df35c8dbc9733a1c9e7aec41e
Mohamed Atef <proofesser@gmail.com>
2024-09-06 15:35:20 -0700

    Added Map to the screen

--- lib/app/presentation/main.dart
+++ lib/app/presentation/main.dart
@@ -14,10 +14,7 @@
 // }
 
 import 'package:fluterprojects/app/presentation/routes/routes.dart';
-import 'package:fluterprojects/app/presentation/widgets/submit_button.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/app/presentation/pages/map_screen.dart
+++ lib/app/presentation/pages/map_screen.dart
@@ -1,12 +1,29 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatelessWidget{
   const MapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context); // Navigate back to the previous screen
+          },
+        ),
+      ),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(30.59965287125796, 32.29632067501643), // Initial position (e.g., San Francisco)
+          zoom: 13,
+        ),
+        myLocationEnabled: true,
+        // Add other properties as needed
+      ),
+    );
   }
-
 }
