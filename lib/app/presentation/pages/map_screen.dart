405a31367516353dcf582b6bd3046d3a94b70c07
Mohamed Atef <proofesser@gmail.com>
2024-09-06 15:38:14 -0700

    Convert Stateless to Stateful Widget for Map Changes

--- lib/app/presentation/pages/map_screen.dart
+++ lib/app/presentation/pages/map_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapScreen extends StatelessWidget{
+class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen>{
 
   @override
   Widget build(BuildContext context) {
