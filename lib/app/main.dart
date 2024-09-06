313f934f1faf9f6cf19064db8cf467fd606b5eaa
Mohamed Atef <proofesser@gmail.com>
2024-09-06 09:55:07 -0700

    Added Column and Row

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HomePage(),
+      home: HorizontalListItems(),
     );
   }
 }
@@ -45,4 +45,55 @@ class HomePage extends StatelessWidget {
       )),
     );
   }
+}
+
+/*
+list with column
+ */
+class ListItems extends StatelessWidget{
+  const ListItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Column(
+        children: [
+          Text("Name"),
+          Text("Date of Birth"),
+          Text("Age"),
+          Text(
+            "Hello",
+            style: TextStyle(
+                fontSize: 28, color: Colors.red, fontWeight: FontWeight.w500),
+          )
+        ],
+      )),
+    );
+  }
+
+}
+
+/*
+horizontal list
+ */
+
+class HorizontalListItems extends StatelessWidget{
+  const HorizontalListItems({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(child: Row(children: [
+        Text("Name"),
+        Text("Date of Birth"),
+        Text("Age"),
+        Text(
+          "Hello",
+          style: TextStyle(
+              fontSize: 28, color: Colors.red, fontWeight: FontWeight.w500),
+        )
+      ],)),
+    );
+  }
+
 }
