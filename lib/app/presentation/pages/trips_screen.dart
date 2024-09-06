af66231f6aca224cd4fbe22c0dd598512e5415d0
Mohamed Atef <proofesser@gmail.com>
2024-09-06 14:33:29 -0700

    Added Trips Screen with Back Button+

--- lib/app/presentation/pages/trips_screen.dart
+++ lib/app/presentation/pages/trips_screen.dart
@@ -1,8 +1,4 @@
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-
 import '../widgets/trip_cart.dart';
 
 class TripsScreen extends StatelessWidget{
